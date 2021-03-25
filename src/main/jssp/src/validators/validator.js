e9a7d1ed3601a624691f1a8f7deed1070d3f5e55
zyx <zhang.yuanxiu@purvar.co.jp>
2021-03-25 17:02:00 +0900

    no message

---
+++ src/main/jssp/src/validators/validator.js
@@ -0,0 +1,17 @@
+/**
+ * 画面制御
+ */
+
+var init = {
+            // getElemntbyName 
+	'prNo': {
+            // エラーメッセージ内容
+    caption: "PRNo.",
+    		// 判断条件
+    required: true
+	},
+	'jyusyu':{
+	caption:"受注No.",
+	numeric: true
+	}
+};
