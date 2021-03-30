00dec3abd083be6e0ec978acb441709b560d110b
zyx <zhang.yuanxiu@purvar.co.jp>
2021-03-30 15:30:49 +0900

    no message

--- src/main/jssp/src/orderList/js/D211.js
+++ src/main/jssp/src/orderList/js/D211.js
@@ -5,10 +5,10 @@ var $bind = {};
  
 function init(request) {
 	 $bind.listtable = [
-     {"col1":"1001", "col2":"明細1", "col3":"なし","col4":"￥200,000","col5":'<input type="checkbox" name="aaa1"> あり'},
-     {"col1":"1002", "col2":"明細2", "col3":"なし","col4":"￥350,000","col5":'<input type="checkbox" name="aaa2" > あり'},
-     {"col1":"1003", "col2":"明細3", "col3":"なし","col4":"￥840,000","col5":'<input type="checkbox" > あり'},
-     {"col1":"1004", "col2":"明細4", "col3":"あり","col4":"￥100,000","col5":'<input type="checkbox" > あり'},
+     {"col1":"1001", "col2":"明細1", "col3":"なし","col4":"200,000","col5":'<input type="checkbox" name="aaa1"> あり'},
+     {"col1":"1002", "col2":"明細2", "col3":"なし","col4":"350,000","col5":'<input type="checkbox" name="aaa2" > あり'},
+     {"col1":"1003", "col2":"明細3", "col3":"なし","col4":"840,000","col5":'<input type="checkbox" > あり'},
+     {"col1":"1004", "col2":"明細4", "col3":"あり","col4":"100,000","col5":'<input type="checkbox" > あり'},
  ];
 }
 
