2657384bb3af8a704ce53309c34b523056536c46
zyx <zhang.yuanxiu@purvar.co.jp>
2021-04-02 18:16:02 +0900

    no message

--- src/main/jssp/src/orderList/js/D120.js
+++ src/main/jssp/src/orderList/js/D120.js
@@ -2,6 +2,110 @@
  * 
  */
 var $bind = {};
+
+var yearList = [
+    {
+        label: "",
+        value: "0"
+    },
+    {
+        label: "2021",
+        value: "2021",
+        selected: true
+    },
+    {
+        label: "2022",
+        value: "2022"
+    },
+    {
+        label: "2023",
+        value: "2023"
+    },
+    {
+        label: "2024",
+        value: "2024"
+    },
+    {
+        label: "2025",
+        value: "2025"
+    },
+    {
+        label: "2026",
+        value: "2026"
+    },
+    {
+        label: "2027",
+        value: "2027"
+    },
+    {
+        label: "2028",
+        value: "2028"
+    },
+    {
+        label: "2029",
+        value: "2029"
+    },
+    {
+        label: "2030",
+        value: "2030"
+    }
+];
+
+var monthList = [
+    {
+        label: "",
+        value: "0"
+    },
+    {
+        label: "01",
+        value: "01"
+    },
+    {
+        label: "02",
+        value: "02"
+    },
+    {
+        label: "03",
+        value: "03"
+    },
+    {
+        label: "04",
+        value: "04",
+        selected: true
+    },
+    {
+        label: "05",
+        value: "05"
+    },
+    {
+        label: "06",
+        value: "06"
+    },
+    {
+        label: "07",
+        value: "07"
+    },
+    {
+        label: "08",
+        value: "08"
+    },
+    {
+        label: "09",
+        value: "09"
+    },
+    {
+        label: "10",
+        value: "10"
+    },
+    {
+        label: "11",
+        value: "11"
+    },
+    {
+        label: "12",
+        value: "12"
+    }
+];
  
 function init(request) {
 	 $bind.listtable = [
