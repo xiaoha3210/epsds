7947f8ff8fcbefcbea4b4b66b1cf0b2da6e99b0d
zyx <zhang.yuanxiu@purvar.co.jp>
2021-05-26 16:47:50 +0900

    売上部署コード追加

--- DBMarkdown/sql/2_受注明細情報.sql
+++ DBMarkdown/sql/2_受注明細情報.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[sh_t_order_details] (
   [project_number] nvarchar(255) not null
   , [order_number] nvarchar(255) not null
+  , [sales_department_cd] nvarchar(100) default NULL
   , [detail_number] nvarchar(255) default NULL
   , [details] nvarchar(1000) default NULL
   , [order_amount_before_apportionment] int default NULL
@@ -17,6 +18,7 @@ CREATE TABLE [dbo].[sh_t_order_details] (
 EXEC sys.sp_addextendedproperty  @name=N'MS_Description',@value=N'受注明細情報',@level0type=N'SCHEMA',@level0name=N'dbo',@level1type=N'TABLE',@level1name=N'sh_t_order_details'
 EXEC sys.sp_addextendedproperty  @name=N'MS_Description',@value=N'PRNo.',@level0type=N'SCHEMA',@level0name=N'dbo',@level1type=N'TABLE',@level1name=N'sh_t_order_details',@level2type=N'COLUMN',@level2name=N'project_number'
 EXEC sys.sp_addextendedproperty  @name=N'MS_Description',@value=N'受注No.',@level0type=N'SCHEMA',@level0name=N'dbo',@level1type=N'TABLE',@level1name=N'sh_t_order_details',@level2type=N'COLUMN',@level2name=N'order_number'
+EXEC sys.sp_addextendedproperty  @name=N'MS_Description',@value=N'売上部署ID',@level0type=N'SCHEMA',@level0name=N'dbo',@level1type=N'TABLE',@level1name=N'sh_t_order_details',@level2type=N'COLUMN',@level2name=N'sales_department_cd'
 EXEC sys.sp_addextendedproperty  @name=N'MS_Description',@value=N'明細No.',@level0type=N'SCHEMA',@level0name=N'dbo',@level1type=N'TABLE',@level1name=N'sh_t_order_details',@level2type=N'COLUMN',@level2name=N'detail_number'
 EXEC sys.sp_addextendedproperty  @name=N'MS_Description',@value=N'明細内容',@level0type=N'SCHEMA',@level0name=N'dbo',@level1type=N'TABLE',@level1name=N'sh_t_order_details',@level2type=N'COLUMN',@level2name=N'details'
 EXEC sys.sp_addextendedproperty  @name=N'MS_Description',@value=N'受注金額(按分前)',@level0type=N'SCHEMA',@level0name=N'dbo',@level1type=N'TABLE',@level1name=N'sh_t_order_details',@level2type=N'COLUMN',@level2name=N'order_amount_before_apportionment'
