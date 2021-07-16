86204f022ce33d7139a01497ba850783cc1bf5b9
lu_weicong <lu_weicong@purvar.co.jp>
2021-07-16 11:40:42 +0900

    6_sh_t_progress_user 追加

---
+++ DBMarkdown/1_sql_without_foreign_key/6_sh_t_progress_user.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[sh_t_progress_user](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[username] [varchar](50) NOT NULL,
+	[password] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_sh_t_progress_user] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
---
+++ DBMarkdown/2_create_data/6_create_user.sql
@@ -0,0 +1,1 @@
+INSERT INTO [dbo].[sh_t_progress_user]  ([username] ,[password]) VALUES('progress_sync', 'progress_sync')
